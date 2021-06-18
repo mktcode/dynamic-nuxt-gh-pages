5afcce0343346b79f23667b9821609730f5fc4cc
mktcode <kontakt@markus-kottlaender.de>
2021-06-18 11:02:59 +0200

    9ca7bbc

---
+++ _nuxt/89d14b2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{203:function(e,n,t){"use strict";t.r(n);var l=t(34),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Post: "+e._s(e.$route.params.slug))])}),[],!1,null,null,null);n.default=component.exports}}]);
